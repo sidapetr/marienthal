71b8dfc0e76e54de3b31bb9be191fe09707a994f
sidapetr <sidapetr@fit.cvut.cz>
2020-05-11 00:11:00 +0200

    basic layout

--- footer.php
+++ footer.php
@@ -1,8 +1,15 @@
-</main>
-<footer>
-    <div class="h-100 row align-items-center">
-        <div>Created by sidp00 as school project in 2020</div>
-    </div>
-</footer>
-</body>
+            </main>
+            <footer>
+                <p>Created by sidp00 in 2020 |
+                    <a href="http://network-marienthal.eu/"> Seminar web pages</a>
+                </p>
+            </footer>
+        </div>
+            <nav>
+                <ul>
+                    <a href="login.php"><li>sign in</li></a>
+                    <a href="register.php"><li>sign up</li></a>
+                </ul>
+            </nav>
+    </body>
 </html>
--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title><?php echo (!empty($pageTitle)?$pageTitle.' - ':'')?>Marienthal</title>
+    <title><?php echo (!empty($pageTitle)?$pageTitle.' - ':'')?>Marienthal workshop app</title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <meta name="author" content="sidp00">
+    <meta name="keywords" content="Marienthal, intercultural, seminar, workshop">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<header class="container bg-dark">
-    <h1 class="text-white py-4 px-2">Apply for a workshop!</h1>
-</header>
-<main class="container">
+    <div id="container">
+        <main>
