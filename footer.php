3afd477bffb00e9a2926148173372ccdf8801cd3
sidapetr <sidapetr@fit.cvut.cz>
2020-05-10 21:38:26 +0200

    connection to mariaDB

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="h-100 row align-items-center">
+    <p>Created by sidp00 as school project in 2020</p>
+</footer>
+</main>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo 'lorem ipsum dolor sit';
+require_once 'db.php';
+include 'header.php';
+?>
+
+
+<p>this is an index page</p>
+
+
+<?php
+include 'footer.php';
