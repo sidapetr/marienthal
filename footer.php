170714b4b3c7d20f43f4f66548d248b435a4f402
sidapetr <sidapetr@fit.cvut.cz>
2020-05-10 21:42:36 +0200

    Footer change

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 </main>
 <footer>
     <div class="h-100 row align-items-center">
-        <p>Created by sidp00 as school project in 2020</p>
+        <div>Created by sidp00 as school project in 2020</div>
     </div>
 </footer>
 </body>
